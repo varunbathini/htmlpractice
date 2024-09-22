8b1cf89e48a191176717db4417340800cfe0b620
vaarun2104 <varunrajbathini@gmail.com>
2024-09-22 19:07:10 +0530

    updated sample routes

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def ping():
-    return "ping"
+    return "ping sample"
+
+@app.route("/home")
+def sample_page():
+    resp = Response("""
+    <html>
+    <body>
+    <h1>This is a HTML based content</h1>
+    </body>
+    </html>
+    """)
+    return resp
 
 app.run()
